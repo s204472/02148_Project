1afdf7de03cfc01cd06624ee3c667d473b1ebd65
Emil-krarup <emil.krarup@live.dk>
2022-01-06 14:48:40 +0100

    javaFx

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module jSpace.Project {
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires javafx.graphics;
+    requires javafx.web;
+    requires javafx.swing;
+    requires java.sql;
+
+    opens common.src.main;
+}
