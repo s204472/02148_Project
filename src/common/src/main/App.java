1b8284af03b62e08bb693414fb1b11105f82d4b8
MagnusSiegumfeldt <45620602+MagnusSiegumfeldt@users.noreply.github.com>
2022-01-07 09:49:45 +0100

    Working comm between server and two players

--- src/common/src/main/App.java
+++ src/common/src/main/App.java
@@ -1,14 +1,39 @@
 package common.src.main;
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import org.jspace.*;
 
 public class App {
 	public static void main(String[] args) {
-		new Thread(new A()).start();
+
+		String port = "9000";
+		String host = "localhost";
+
+		String uri = "tcp://" + host + ":" + port + "/?conn";
+
+		SpaceRepository repo = new SpaceRepository();
+		repo.addGate(uri);
+		Space serverToPlayer = new SequentialSpace();
+		Space playerToServer = new SequentialSpace();
+		Space id = new SequentialSpace();
+
+		try {
+			id.put(1);
+			id.put(2);
+		} catch (InterruptedException e) {}
+
+		repo.add("serverToPlayer", serverToPlayer);
+		repo.add("playerToServer", playerToServer);
+		repo.add("id", id);
+
+
+		while(true){
+			try {
+
+				Object[] objects = playerToServer.get(new FormalField(String.class), new FormalField(Integer.class));
+				System.out.println(objects[0] + (objects[1].toString()));
+
+				//here the board is created
+			} catch (InterruptedException e) {}
+		}
 	}
 }
 
